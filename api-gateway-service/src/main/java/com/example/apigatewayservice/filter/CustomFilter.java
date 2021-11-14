7bf335e49ee441b2a8d97615e7e6743668998d11
gunkim <gunkim.dev@gmail.com>
2021-11-14 21:12:00 +0900

    refactor: logger 메시지 오타 수정

--- api-gateway-service/src/main/java/com/example/apigatewayservice/filter/CustomFilter.java
+++ api-gateway-service/src/main/java/com/example/apigatewayservice/filter/CustomFilter.java
@@ -26,7 +26,7 @@ public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Conf
 
             // Custom Post Filter
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                log.info("Custom POST filter: request code -> {}", response.getStatusCode());
+                log.info("Custom POST filter: response code -> {}", response.getStatusCode());
             }));
         };
     }
